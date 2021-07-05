892aae91c17de9c59d0b2739457f74d00c73782e
Antonin Dudermel <antonin.dudermel@caramail.fr>
2021-07-05 16:01:11 +0200

    added todo and continued datatypes

--- datatypes.py
+++ datatypes.py
@@ -4,22 +4,42 @@
 """all the datatypes used in the downloader and the webpages generator"""
 
 
+class Episode():
+    """internal representation of an episode"""
+    def __init__(self, title="", name="", origin="", pic_list=None):
+        pass
+
+    def generate_soup(self, style):
+        """generate a local soup from the episode"""
+
+    @classmethod
+    def from_local_episode(cls, soup):
+        """initialize a Comic from the soup of a local comic page"""
+
+
 class Comic():
-    """a class for whole comics"""
-    def __init__(self, title="", name="", origin="", ep_list=None):
+    """internal representation of whole comics"""
+    def __init__(self, scraper, title="", name="", ep_list=None):
         pass
 
     def generate_soup(self, style, inlined=False):
-        """generate a soup from the comic"""
-
-    @classmethod
-    def from_webtoon_episode(cls, soup):
-        """initialize a Comic from the soup of a webtoon episode page"""
-
-    @classmethod
-    def from_webtoon_comic(cls, soup):
-        """initialize a Comic from the soup of a webtoon comic page"""
+        """generate a local soup from the comic"""
 
     @classmethod
     def from_local_comic(cls, soup):
         """initialize a Comic from the soup of a local comic page"""
+
+
+class Scraper():
+    """an abstract class for scraping comics from webpages"""
+    def __init__(self, source, cache=True):
+        raise NotImplementedError
+
+    def comic_data(self):
+        """get the metadata of the comic"""
+
+    def episode_data(self, ep_no):
+        """get the metadata of an episode of the comic"""
+
+    def download_episode(self, ep_no):
+        """download the pictures of an episode"""
